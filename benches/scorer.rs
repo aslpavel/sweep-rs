8b2e67460bb4d2cbaa2e725ba7c9487e6f9a7a7f
Pavel Aslanov <asl.pavel@gmail.com>
2020-10-25 00:01:27 +0100

    [bench] fix benchmark imports

--- benches/scorer.rs
+++ benches/scorer.rs
@@ -1,5 +1,5 @@
 use criterion::{criterion_group, criterion_main, Criterion, Throughput};
-use sweep_lib::{score::KMPPattern, Candidate, FuzzyScorer, Haystack, Scorer, SubstrScorer};
+use sweep_lib::{scorer::KMPPattern, Candidate, FuzzyScorer, Haystack, Scorer, SubstrScorer};
 
 pub fn scorer_benchmark(c: &mut Criterion) {
     let candidate = String::from("./benchmark/target/release/.fingerprint/semver-parser-a5e84da67081840e/test/lib-semver_parser-a5e84da67081840e.json");
